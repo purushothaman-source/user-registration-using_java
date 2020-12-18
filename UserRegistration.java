e7f93251463f7264ac05c4e6558d32dd24062678
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-18 08:41:19 +0530

    ADDED :welcome message

---
+++ UserRegistration.java
@@ -0,0 +1,8 @@
+package com.java.userregistration;
+
+public class UserRegistration {
+	public static void main(String[] args) {
+		System.out.println("Welcome to User-Registration problem using Java");
+
+	}
+}
