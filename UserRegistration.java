549e0b60cb9ba2bb6a3fb533aea125f327093090
Purushothaman-source <Purushoth99jun@gmail.com>
2020-12-18 09:58:28 +0530

    ADDED :firstName pattern

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,8 +1,25 @@
 package com.java.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 	public static void main(String[] args) {
-		System.out.println("Welcome to User-Registration problem using Java");
-
+		Scanner input = new Scanner(System.in);
+		String firstName = "^[A-Z][a-zA-Z]{2,}$";
+		Pattern pattern = Pattern.compile(firstName);
+		System.out.println("Enter valid FirstName : ");
+		String text;
+		while (true) {
+			text = input.nextLine();
+			Matcher matcher = pattern.matcher(text);
+			if (matcher.matches()) {
+				System.out.print("Valid pattern");
+				break;
+			} else {
+				System.out.print("Invalid Pattern,Try Again");
+			}
+		}
 	}
 }
